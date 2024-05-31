d5102306bc2fb83614e950922f86fb5dfc73df3c
Jose Saarimaa <jose.saarimaa@stat.fi>
2024-05-31 10:41:23 +0300

    Added snapshot tests for the workseet xml builder. Removed the old xlsx npm package.

--- src/core/tables/xlsx/xlsxWorksheetBuilder.test.ts
+++ src/core/tables/xlsx/xlsxWorksheetBuilder.test.ts
@@ -1,5 +1,19 @@
+import { XLSX_FIXTURES_COMPLEX_VIEW_ROW_AND_COL_VARS } from "../fixtures/xlsxFixtures";
+import { buildXMLSheet } from "./xlsxWorksheetBuilder";
+
 describe('Worksheet xml generation tests', () => {
-    it('Should generate expexted xml output with the provided fixture', () => {
-        expect(false).toBe(true); // TODO
+    it('Should generate expexted xml output with the provided fixture with fi locale', () => {
+        const sheet : string = buildXMLSheet(XLSX_FIXTURES_COMPLEX_VIEW_ROW_AND_COL_VARS['fi']);
+        expect(sheet).toMatchSnapshot();
+    });
+
+    it('Should generate expexted xml output with the provided fixture with sv locale', () => {
+        const sheet : string = buildXMLSheet(XLSX_FIXTURES_COMPLEX_VIEW_ROW_AND_COL_VARS['sv']);
+        expect(sheet).toMatchSnapshot();
+    });
+
+    it('Should generate expexted xml output with the provided fixture with en locale', () => {
+        const sheet : string = buildXMLSheet(XLSX_FIXTURES_COMPLEX_VIEW_ROW_AND_COL_VARS['en']);
+        expect(sheet).toMatchSnapshot();
     });
 });
