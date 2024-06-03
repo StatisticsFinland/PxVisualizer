3261f2b52d2d05f8192c225546355dc9d3e4be3a
Sakari Malkki <sakari.malkki@stat.fi>
2024-06-03 14:00:11 +0300

    Remove tests, exclude stories

--- src/coverageTestFunctions.test.ts
+++
@@ -1,7 +0,0 @@
-import { coveredTestFunction } from './coverageTestFunctions';
-
-describe('coveredTestFunction test', () => {
-    it('should return the input multiplied by 2', () => {
-        expect(coveredTestFunction(3)).toBe(6);
-    });
-});
--- src/coverageTestFunctions.ts
+++
@@ -1,7 +0,0 @@
-export function coveredTestFunction(input: number): number {
-    return input * 2;
-}
-
-export function uncoveredTestFunction(input: number): number {
-    return input * 3;
-}
