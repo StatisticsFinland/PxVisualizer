2fd8dbd73d62f0bd3bf326836d0c11521683e865
Sakari Malkki <sakari.malkki@stat.fi>
2024-12-02 10:29:28 +0200

    Uses data label formatter for precision
    (cherry picked from commit 539f5edbf30026d581abebcf6d6b1a9ab0f35989)

--- src/core/chartOptions/chartOptions.ts
+++ src/core/chartOptions/chartOptions.ts
@@ -1,6 +1,6 @@
 import { Options, YAxisOptions } from 'highcharts';
 import { View } from "../types/view";
-import { getAxisLabelShorteningFunction, getFormattedUnits, getToolTipFormatterFunction, getScreenReaderFormatterCallbackFunction } from './Utility/formatters';
+import { getAxisLabelShorteningFunction, getFormattedUnits, getToolTipFormatterFunction, getScreenReaderFormatterCallbackFunction, getDataLabelFormatterFunction } from './Utility/formatters';
 import { Translations } from '../conversion/translations';
 import { buildHighchartSeries } from './Utility/seriesDataBuilder';
 import {  getXAxisOptions } from './Utility/timeIntervals';
@@ -40,7 +40,8 @@ export const commonDatalabelsOptions = (view: View, locale: string) => {
 
     const dataLabelOptions = {
         enabled: view.visualizationSettings.showDataPoints,
-        style: dataValueLabelStyle
+        style: dataValueLabelStyle,
+        formatter: getDataLabelFormatterFunction(locale)
     }
     return dataLabelOptions;
 }
