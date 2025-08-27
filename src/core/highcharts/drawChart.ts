7ff986f9af61cd490c965627cdda313042d0ddfd
Sakari Malkki <sakari.malkki@stat.fi>
2025-08-27 14:12:50 +0300

    Conditional module imports for drawChart

--- src/core/highcharts/drawChart.ts
+++ src/core/highcharts/drawChart.ts
@@ -1,8 +1,4 @@
 import * as Highcharts from "highcharts";
-import 'highcharts/modules/pattern-fill.js';
-import 'highcharts/modules/accessibility.js';
-import 'highcharts/modules/exporting.js';
-import 'highcharts/modules/offline-exporting.js';
 import { convertPxGraphDataToChartOptions } from "../conversion";
 import { IQueryVisualizationResponse } from "../types";
 import { defaultTheme } from "./themes";
@@ -12,6 +8,20 @@ import { convertPxGrafResponseToView } from "../conversion/viewUtils";
 import { formatLocale } from "../chartOptions/Utility/formatters";
 import { IChartOptions } from "../types/chartOptions";
 
+// Only load Highcharts modules in a browser environment
+const loadHighchartsModules = () => {
+    if (typeof window !== 'undefined') {
+        try {
+            require('highcharts/modules/pattern-fill.js');
+            require('highcharts/modules/accessibility.js');
+            require('highcharts/modules/exporting.js');
+            require('highcharts/modules/offline-exporting.js');
+        } catch (e) {
+            console.error('Error loading Highcharts modules:', e);
+        }
+    }
+};
+
 export const drawChart = (
     container: string,
     pxGraphData: IQueryVisualizationResponse,
@@ -19,6 +29,7 @@ export const drawChart = (
     selectedVariableCodes: TVariableSelections | null = null,
     options: IChartOptions) =>
 {
+    loadHighchartsModules();
     const validLocale = formatLocale(locale);
     Highcharts.setOptions(defaultTheme(validLocale));
     const variableSelections = extractSelectableVariableValues(pxGraphData.selectableVariableCodes, pxGraphData.metaData, pxGraphData.visualizationSettings.defaultSelectableVariableCodes, selectedVariableCodes);
